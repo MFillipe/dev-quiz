27c42321d9cf46568fc7262f41661855f7dc49de
Matheus Filipe <matheus_filipe00@outlook.com>
2021-04-20 10:26:15 -0300

    fix: space between components

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:DevQuiz/core/core.dart';
 import 'package:flutter/material.dart';
 
@@ -23,19 +21,19 @@ class QuizCardWidget extends StatelessWidget {
             child: Image.asset(AppImages.blocks),
           ),
           SizedBox(
-            height: 24,
+            height: 20,
           ),
           Text(
             "Gerenciamento de Estado",
             style: AppTextStyles.heading15,
           ),
           SizedBox(
-            height: 24,
+            height: 20,
           ),
           Row(
             children: [
               Expanded(
-                  flex: 1, child: Text("3 de 10", style: AppTextStyles.body11)),
+                  flex: 1, child: Text("3/10", style: AppTextStyles.body11)),
               Expanded(
                 flex: 4,
                 child: LinearProgressIndicator(
